ab312de07e3c554fb65dd0fc1828392c0870410a
Timofei Drobotenko <timon.atm@gmail.com>
2026-04-17 22:16:59 +0300

    добавил тесты в проект

---
+++ src/test/java/tests/first/PositiveTests.java
@@ -0,0 +1,21 @@
+package tests.first;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    void someTest() {assertTrue(true);}
+
+    @Test
+    void someTest1() {assertTrue(true);}
+
+    @Test
+    void someTest2() {assertTrue(true);}
+
+    @Test
+    void someTest3() {assertTrue(true);}
+
+}
---
+++ src/test/java/tests/first/SkippedTests.java
@@ -0,0 +1,20 @@
+package tests.first;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void someTest() {assertTrue(true);}
+
+    @Test
+    @Disabled("Some reason")
+    void someTest1() {assertTrue(true);}
+
+
+}
